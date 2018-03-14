bac3c2195b7c03ce878b973dadda83c82028caa0
Eugene <igenexxx@gmail.com>
2018-03-14 23:26:13 +0300

    6 from 10

--- src/index.js
+++ src/index.js
@@ -1,20 +1,78 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+/*const createEnumerableProperty = (obj) => {
+  "use strict";
+  Object.defineProperty(this, obj, {
+    enumerable: true,
+    configurable: true,
+    writable: true,
+    value: 'static'
+  });
+};*/
+
+const createNotEnumerableProperty = (propertyName) => {
+  Object.defineProperty(Object.prototype, propertyName, {
+    configurable: true,
+    enumerable: false,
+    set (value) {
+      propertyName = value
+    },
+    get () {
+      return propertyName
+    }
+  });
+  return propertyName;
+};
+
+const createProtoMagicObject = () => {
+  function Temp(){}
+  let temp = new Temp;
+  temp.prototype = new Temp();
+  return temp;
+};
+
+
+const incrementor = () => {
+  return (function () {
+    incrementor.counter = ++incrementor.counter || 1;
+    return function incr() {
+      incrementor.counter++;
+      incr.valueOf = incrementor.counter;
+      return incr;
+    }
+  })();
+};
+
+
+const asyncIncrementor = () => {
+  asyncIncrementor.counter = ++asyncIncrementor.counter || 0;
+  return new Promise((resolve) => {
+    resolve(asyncIncrementor.counter);
+  });
+};
+
+const createIncrementer = () => {
+  let arr = [];
+  arr.push(1,2,3,4,5,6,7,8,9);
+  arr = arr[Symbol.iterator]();
+
+  return arr;
+
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = () => {
+};
+const getDeepPropertiesCount = () => {
+};
+const createSerializedObject = () => {
+};
+const toBuffer = () => {
+};
+const sortByProto = () => {
+};
 
-exports.createEnumerableProperty = createEnumerableProperty;
+// exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
 exports.createProtoMagicObject = createProtoMagicObject;
 exports.incrementor = incrementor;
@@ -23,4 +81,4 @@ exports.createIncrementer = createIncrementer;
 exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
-exports.sortByProto = sortByProto;
+exports.sortByProto = sortByProto
